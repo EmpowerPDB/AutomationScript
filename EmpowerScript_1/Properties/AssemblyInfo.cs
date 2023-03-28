91852650e1dfefd4bcdef012b9c87d7832bd59b1
Pedro Debevere <pedro.debevere@skyline.be>
2023-03-28 14:11:13 +0200

    Add project files.

---
+++ EmpowerScript_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("EmpowerScript_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("EmpowerScript_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("E2C7EB9B-B729-40AC-8854-DA75D40F6B84")]
+[assembly: AssemblyVersion("1.0.0.0")]
